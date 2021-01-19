a2b23ff9895bc63bd4fa08e54e8cf85df4b96f69
Kate Peterson <petersonkatec@gmail.com>
2021-01-19 04:10:31 +0000

    fixed formatting

--- formatting.py
+++ formatting.py
@@ -11,14 +11,16 @@ def formatMessage(string, name):
   string = string.replace("{NAME}", name.upper())
   stringTemp = randomizeEmphasis(string.split("http")[0])
   if (len(string.split("http")) > 1):
-    string = stringTemp + "http" + string.split("http")[1]
+    string = stringTemp + " http" + string.split("http")[1]
   else:
     string = stringTemp
   return string
 
 def randomizeEmphasis(string):
-  formattingString = random.choice(italics) + random.choice(bold) + random.choice(underline)
-  string = formattingString + string + formattingString
+  italicsString = random.choice(italics)
+  boldString = random.choice(bold)
+  underlineString = random.choice(underline)
+  string = underlineString + boldString + italicsString + string + italicsString + boldString + underlineString
   caps = random.randint(0, 2)
   if (caps == 0):
     string = string.upper()
